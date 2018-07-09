a8fab63e0988648b39a37a2c2905de08f1e5e4db
Ekaterina Korovina <splendidavocado@fb.com>
2018-07-09 14:43:32 -0700

    1

---
+++ app/src/main/java/com/example/splendidavocado/splendid_insta/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.splendidavocado.splendid_insta;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
